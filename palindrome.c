d58ac612c56a15aae22c71471a22282b16de7186
JennyBacab <78243042+JennyBacab@users.noreply.github.com>
2021-03-15 22:17:16 -0600

    Add files via upload

---
+++ palindrome.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<string.h>
+
+void Palindrome(char userInput[50]){
+	int i;
+	int j;
+	int largo = strlen(userInput);
+	int value;
+	for(i=0; i<=largo;i++){
+		for(j=largo; j>=0;j--){
+			if(userInput[i]=userInput[j]){
+				value=1;
+			}else
+				value=0;
+		}
+		
+	}printf("%i", value);	
+}
+	
+	
+	int main(void) {
+		char userInput[50];
+		scanf("%s", &userInput);
+		Palindrome(userInput);
+		return 0;
+	}
